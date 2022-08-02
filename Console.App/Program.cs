999c3f1311fbe9954867fd28ffcbfe9e47b68d7f
Vadim <6ddd14@gmail.com>
2022-08-02 19:54:21 +0300

    Добавьте файлы проекта.

---
+++ Console.App/Program.cs
@@ -0,0 +1,16 @@
+﻿
+bool word = true;
+do
+{
+    if (int.TryParse(Console.ReadLine(), out var number))
+    {
+        Console.Write($"{number}");
+        word = false;
+    }
+} while (word);
+
+ 
+    
+
+
+
